2251d91efb1314967e82bf4237fa7799f87ef5d5
sobrock12 <59989171+sobrock12@users.noreply.github.com>
2021-06-28 06:28:39 -0500

    2nd commit
    -added functionality to several pages

---
+++ view/quote.php
@@ -0,0 +1,145 @@
+<?php
+
+    include('header.php');
+
+?>
+
+<div class="container-fluid">
+
+    <div class="row">
+
+        <div class="col-xs-3">
+
+            <input type="button" class="myButton" value="Search Inventory..."><br>
+                
+            <table>
+                <tr>
+                    <th>Make</th>
+                    <th>Model</th>
+                    <th>System</th>
+                <tr>
+                <tr>
+                    <td>Garmin</td>
+                    <td>Controller</td>
+                    <td>GFC500</td>
+                </tr>
+                <tr>
+                    <td>Garmin</td>
+                    <td>Controller</td>
+                    <td>GFC500</td>
+                </tr>
+                <tr>
+                    <td>Garmin</td>
+                    <td>Controller</td>
+                    <td>GFC500</td>
+                </tr>
+                <tr>
+                    <td>Garmin</td>
+                    <td>Controller</td>
+                    <td>GFC500</td>
+                </tr>
+                <tr>
+                    <td>Garmin</td>
+                    <td>Controller</td>
+                    <td>GFC500</td>
+                </tr>
+                <tr>
+                    <td>Garmin</td>
+                    <td>Controller</td>
+                    <td>GFC500</td>
+                </tr>
+                <tr>
+                    <td>Garmin</td>
+                    <td>Controller</td>
+                    <td>GFC500</td>
+                </tr>
+                <tr>
+                    <td>Garmin</td>
+                    <td>Controller</td>
+                    <td>GFC500</td>
+                </tr>
+                <tr>
+                    <td>Garmin</td>
+                    <td>Controller</td>
+                    <td>GFC500</td>
+                </tr>
+                <tr>
+                    <td>Garmin</td>
+                    <td>Controller</td>
+                    <td>GFC500</td>
+                </tr>
+                <tr>
+                    <td>Garmin</td>
+                    <td>Controller</td>
+                    <td>GFC500</td>
+                </tr>
+                <tr>
+                    <td>Garmin</td>
+                    <td>Controller</td>
+                    <td>GFC500</td>
+                </tr>
+                <tr>
+                    <td>Garmin</td>
+                    <td>Controller</td>
+                    <td>GFC500</td>
+                </tr>
+                <tr>
+                    <td>Garmin</td>
+                    <td>Controller</td>
+                    <td>GFC500</td>
+                </tr>
+                <tr>
+                    <td>Garmin</td>
+                    <td>Controller</td>
+                    <td>GFC500</td>
+                </tr>
+                <tr>
+                    <td>Garmin</td>
+                    <td>Controller</td>
+                    <td>GFC500</td>
+                </tr>
+                <tr>
+                    <td>Garmin</td>
+                    <td>Controller</td>
+                    <td>GFC500</td>
+                </tr>
+                <tr>
+                    <td>Garmin</td>
+                    <td>Controller</td>
+                    <td>GFC500</td>
+                </tr>
+                <tr>
+                    <td>Garmin</td>
+                    <td>Controller</td>
+                    <td>GFC500</td>
+                </tr>
+                <tr>
+                    <td>Garmin</td>
+                    <td>Controller</td>
+                    <td>GFC500</td>
+                </tr>
+
+
+            </table>
+
+            <input type="button" class="myButton" value="Manage Inventory..."><br>
+
+        </div>
+
+        <div class="col-xs-3">
+
+            more stuff
+
+        </div>
+
+        <div class="col-xs-6">
+
+            more more stuff
+
+        </div>
+        
+
+    </div>
+
+
+</div>
--- view/select.php
+++ view/select.php
@@ -6,20 +6,48 @@
 
 <div class="container">
     <div class="row">
-        <div class="col-xs-3">
-            <div class="loginForm">
-                Sort By Column
-            </div>
+        <div class="col-xs-2">
+            
+                <h2>Sort By:</h2>
+                <form>
+                    <input type="radio" id="asc" name="sortChoice" value="asc">
+                    <label for="asc">Ascending</label><br>
+                    <input type="radio" id="desc" name="sortChoice" value="desc">
+                    <label for="desc">Descending</label><br>
+                    <input type="submit" class="mySort" value="Sort!">
+                </form>
+            
         </div>
-        <div class="col-xs-6">
-            <div class="loginForm">
-                Search bar and Table column
+        <div class="col-xs-8">
+            <div class="purpleBack">
+                <input type="text" placeholder="Search...">
+                <button type="submit">Submit</button>
             </div>
+                <table>
+                    <tr>
+                        <th>Customer/Quote Name</th>
+                        <th>Tail #</th>
+                        <th>Contact #</th>
+                        <th>Date Created</th>
+                        <th>Other Info</th>
+                    </tr>
+                    <tr>
+                        <td>Flying Co.</td>
+                        <td>N123XD</td>
+                        <td>123-4567</td>
+                        <td>6/22/21</td>
+                        <td>GFC500</td>
+                    </tr>
+                </table>
         </div>
-        <div class="col-xs-3">
-            <div class="loginForm">
-                load and delete button column
-            </div>
+        <div class="col-xs-2">
+
+            
+                <input type="button" class="myButton" value="Load this Quote!"><br>
+                <input type="button" class="myButton" value="Delete this Quote!"><br>
+                <input type="button" class="myButton" value="Create a new Quote!"><br>
+            
+                
         </div>
     </div>
 </div>
