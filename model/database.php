ddd4641ce2647923d6b0533759318a180434a75c
sobrock12 <59989171+sobrock12@users.noreply.github.com>
2021-08-18 21:20:13 -0500

    added JawsDB connection

--- model/database.php
+++ model/database.php
@@ -3,9 +3,13 @@
 //database connection page
 
 //localhost connection
-$pdo = 'mysql:host=localhost;dbname=quoteProj';
-$username= 'root';
-$password = '';
+//$pdo = 'mysql:host=localhost;dbname=quoteProj';
+//$username= 'root';
+//$password = '';
+
+$pdo = 'mysql:host=pk1l4ihepirw9fob.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=dgzo1i9cdhmlwbqm';
+$username = 'l13z3mmjmtw4hace';
+$password = 'vutx627w3bkh9qff';
 
 try {
     //local development server connection
