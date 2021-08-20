d4cb7a1c8f407666c4047f06fbed331e9b91044b
sobrock12 <59989171+sobrock12@users.noreply.github.com>
2021-08-19 20:10:34 -0500

    set up ClearDB connection

--- model/database.php
+++ model/database.php
@@ -3,14 +3,19 @@
 //database connection page
 
 //localhost connection
-$pdo = 'mysql:host=localhost;dbname=quoteProj';
-$username= 'root';
-$password = '';
+//$pdo = 'mysql:host=localhost;dbname=quoteProj';
+//$username= 'root';
+//$password = '';
+
+//ClearDB connection
+$pdo = 'mysql:host=us-cdbr-east-04.cleardb.com;dbname=heroku_778b37347365d17';
+$username = 'b49c47ccddc431';
+$password = '9b34bf5a';
 
 try {
     //local development server connection
     //if using a $password, add it as 3rd parameter
-    $pdo = new PDO($pdo, $username);
+    $pdo = new PDO($pdo, $username, $password);
 
 } catch (PDOException $e) {
 
